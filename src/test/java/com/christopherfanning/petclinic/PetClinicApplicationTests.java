81e95e5689456b3e56d274a9a52c63a704e62295
christopherfanning <christopherleefanning@gmail.com>
2020-09-01 15:26:30 -0500

    Initialize project with spring and deps. edit .gitignore

---
+++ src/test/java/com/christopherfanning/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christopherfanning.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
